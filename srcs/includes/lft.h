5df3a3b268dec795f5ca795fc15e84e5e3495628
_ipal <malkoleyplay@gmail.com>
2018-11-19 15:56:16 +0200

    al

---
+++ srcs/includes/lft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lft.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/24 10:04:13 by tmaluh            #+#    #+#             */
+/*   Updated: 2018/10/29 15:49:18 by tmaluh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include "lft_str.h"
+# include "lft_lst.h"
+# include "lft_mem.h"
+# include "lft_put.h"
+# include "lft_math.h"
+# include "lft_ctype.h"
+
+
+# define BUFF_SIZE	42
+
+int		ft_gnl(const int fd, char **line);
+
+
+#endif
