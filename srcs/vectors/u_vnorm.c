4d844025564000472058ea69ad3d909692a1ec47
Iipal <malkoleyplay@gmail.com>
2019-06-09 05:17:22 +0300

    improved u_vnorm.c

--- srcs/vectors/u_vnorm.c
+++ srcs/vectors/u_vnorm.c
@@ -6,16 +6,15 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 22:34:31 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/06/09 05:04:20 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/06/09 05:16:27 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-inline t_v4df	u_vnorm(const t_v4df v)
+inline __v4df	u_vnorm(const __v4df v)
 {
 	const double_t	sqrtvdv = sqrt(u_vdot(v, v));
-	const t_v4df	c = {X(v) / sqrtvdv, Y(v) / sqrtvdv, Z(v) / sqrtvdv, 0.0};
 
-	return (c);
+	return ((__v4df){X(v) / sqrtvdv, Y(v) / sqrtvdv, Z(v) / sqrtvdv, 0.0});
 }
