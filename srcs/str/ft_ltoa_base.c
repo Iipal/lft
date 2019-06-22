f1846b3273855158175829bfdc9270206bc3c1be
Iipal <malkoleyplay@gmail.com>
2019-06-22 03:21:27 +0300

    norme ft_ltoa_base

--- srcs/str/ft_ltoa_base.c
+++ srcs/str/ft_ltoa_base.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/21 00:07:31 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/06/21 08:55:12 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/06/21 23:07:46 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 static inline void	add_aux(const int64_t n, const int64_t b,
 						string out, int64_t *const p)
 {
-	char	base[] = "0123456789ABCDEF";
+	char const	base[] = "0123456789ABCDEF";
 
 	if (n <= -b || b <= n)
 		add_aux(n / b, b, out, p);
 	out[(*p)++] = base[ABS(n % b)];
 }
 
-string		ft_ltoa_base(int64_t value, uint8_t base)
+string				ft_ltoa_base(int64_t value, uint8_t base)
 {
 	string	out;
 	int64_t	p;
