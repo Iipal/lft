199542101ffb14afca2bc6cb23178610f61e1b0f
Iipal <malkoleyplay@gmail.com>
2019-08-24 16:02:20 +0300

    __nonnull in libft_mem.h

--- includes/libft_mem.h
+++ includes/libft_mem.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/15 17:08:05 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/08/11 00:56:10 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/08/24 16:01:54 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,24 @@
 **	Erases the data in the \param n bytes of \param s,
 **	 by writing zeros('\0') to that area.
 */
-void	ft_bzero(void *const s, size_t const n);
+void	ft_bzero(void *const s, size_t const n) __nonnull((1));
 
 /*
 **	Free memory at pointer \param ap.
 */
-void	ft_memdel(void **ap);
+void	ft_memdel(void **ap) __nonnull((1));
 
 /*
 **	\return \param dest where fills the first \param len bytes of
 **	 \param dest with the \param c.
 */
-void	*ft_memset(void *const dest, int32_t const c, size_t len);
+void	*ft_memset(void *const dest, int32_t const c, size_t len) __nonnull((1));
 
 /*
 **	\return \param dest where copies \param n bytes from
 **	 \param src to \param dest.
 */
-void	*ft_memcpy(void *dest, void const *const src, size_t const n);
+void	*ft_memcpy(void *dest, void const *const src, size_t const n) __nonnull((1,2));
 
 /*
 **	\return \param dest where copies no more than \param n bytes from
@@ -45,19 +45,19 @@ void	*ft_memcpy(void *dest, void const *const src, size_t const n);
 void	*ft_memccpy(void *const dest,
 			void const *const src,
 			uint8_t const c,
-			size_t const n);
+			size_t const n) __nonnull((1,2));
 
 /*
 **	\return \param dest where copies
 **	\param len bytes from \param src to \param dest.
 */
-void	*ft_memmove(void *const dest, void const *const src, size_t len);
+void	*ft_memmove(void *const dest, void const *const src, size_t len) __nonnull((1,2));
 
 /*
 **	\return matching byte of \param s after scans the initial \param n bytes
 **	 of \param s for the first instance of c.
 */
-void	*ft_memchr(void const *const s, char const c, size_t n);
+void	*ft_memchr(void const *const s, char const c, size_t n) __nonnull((1));
 
 /*
 **	\return pointer to allocated \param size bytes.
@@ -71,6 +71,6 @@ void	*ft_memalloc(size_t const size);
 **	 respectively, to be less than, to match,
 **	 or be greater than the first \param n bytes of \param s2.
 */
-int32_t	ft_memcmp(void const *s1, void const *s2, size_t n);
+int32_t	ft_memcmp(void const *s1, void const *s2, size_t n) __nonnull((1,2));
 
 #endif
