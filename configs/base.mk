51a2965a008709b1304b8bb8a94e4bca2fd4291c
Iipal <malkoleyplay@gmail.com>
2019-12-17 17:03:29 +0200

    update make

--- configs/base.mk
+++ configs/base.mk
@@ -1,12 +1,36 @@
-NAME := libft.a
+.EXPORT_ALL_VARIABLES:
+
+NAME := $(notdir $(CURDIR)).a
 NPWD := $(CURDIR)/$(NAME)
 
-CC     := clang
-
-# all .c files must to placed in to "srcs" folder(or sub-dirs) only.
+# all source code .c files must to places in to "srcs" folder(or sub-dirs) only.
 ifneq (,$(wildcard ./srcs))
-SRCS      := $(shell find srcs -name "*.c")
-OBJS      := $(SRCS:.c=.o)
+SRCS := $(shell find srcs -name "*.c")
+OBJS := $(SRCS:.c=.o)
+ASMS := # reversed for 'assembly' and 'assembly_all' rules where ASMS:=$(SRCS:%.c=%.S) and OBJS:= equals to nothing
+else
+$(error "all source code .c files must to places in to "srcs" folder(or sub-dirs) only.")
+endif
+# all header .h files must be placed in to "includes" folder(or sub-dirs) only.
+ifneq (,$(wildcard ./includes))
+IFLAGS := $(addprefix -I,$(shell find ./includes -type d))
+else
+$(error "all header .h files must be placed in to "includes" folder(or sub-dirs) only.")
 endif
 
-DEL  := rm -rf
+CC              := clang
+CFLAGS          := -Wall -Wextra -Werror -Wunused -MMD
+CFLAGS_DEBUG    := -glldb -Og
+CFLAGS_SANITIZE := $(CFLAGS_DEBUG) -fsanitize=address
+CFLAGS_OPTIMIZE := -march=native -mtune=native -Ofast -pipe -flto -fpic
+CFLAGS_ASSEMBLY := $(filter-out -flto -fpic,$(CFLAGS_OPTIMIZE)) -S -masm=intel
+
+CFLAGS_OPTIONAL := $(CFLAGS_OPTIMIZE)
+
+AR              := ar
+ARFLAGS         := -rcs
+
+UNAME_S ?= $(shell uname -s)
+ifeq ($(UNAME_S),Linux)
+AR := llvm-ar
+endif
